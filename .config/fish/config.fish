7f3df474ef012b758ab64b7579f9726d946b64ee
Fabien Le Frapper <contact@fabienlefrapper.me>
2018-06-06 09:17:59 +0200

    Update aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,21 +1,35 @@
-# . $HOME/.cargo/env
-# set -x --universal fish_user_paths $fish_user_paths ~/.composer/vendor/bin
-
-# set -x LANG en_US.utf-8
-# set RUST_SRC_PATH /Users/lefrapperfabien/.rustup/toolchains/stable-x86_64-apple-darwin/lib/rustlib/src/rust/src
-
+##########
+## TMUX ##
+##########
 alias tmn "tmux new -s"
 alias tma "tmux a -t"
 alias tml "tmux ls"
+
+##############
+## Dotfiles ##
+##############
 alias config "/usr/bin/git --git-dir=$HOME/.pointfichiers/ --work-tree=$HOME"
 
+#########
+## VIM ##
+#########
 alias v "nvim ."
 
+#########
+## Git ##
+#########
 alias gs "git status"
 alias gl "git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
 alias gap "git add . -p"
 alias gcp "git checkout . -p"
 alias gcb "git checkout -b"
+
+############
+## Docker ##
+############
+alias dcu "docker-compose up"
+alias dcs "docker-compose stop"
+alias dck "docker-compose kill"
 
 # Rust
 alias rcm "rustc main.rs"
