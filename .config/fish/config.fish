f766bc4020f4465592818c91cba414b36185a5d8
Fabien Le Frapper <contact@fabienlefrapper.me>
2019-07-08 11:23:59 +0200

    Add fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,5 @@
+eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+set PATH ~/.npm-global $PATH
+# alias pip "pip3"
+umask 002
+set SPACEFISH_PROMPT_ORDER time user dir host pyenv git line_sep char
