c27e0f09bd70e2f80967466f0b08fb1fddd5fa5a
Fabien Le Frapper <contact@fabienlefrapper.me>
2020-12-23 09:45:08 +0100

    Updates

--- .scripts/docker.sh
+++ .scripts/docker.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
-# install docker using convenience script
-# https://docs.docker.com/install/linux/docker-ce/ubuntu/
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   focal \
+   stable"
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
 # install docker compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
